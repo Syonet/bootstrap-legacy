6df79e6468f5ab4afc08093671c91224207f9133
Daniel Tartarotti Sobrosa <danieltsobrosa@gmail.com>
2014-11-18 11:38:13 -0200

    Corrigido problema que fazia a largura das células ficarem iguais
    Isso ocorria mesmo com colspan.

--- src/scripts/pivot-table.js
+++ src/scripts/pivot-table.js
@@ -6,7 +6,8 @@
     // Default options
     options: {
       pivotColWidth: '100px',
-      colWidth: '80px'
+      colWidth: '80px',
+      ignoreColWidth: false
     },
 
     _create: function() {
@@ -45,11 +46,35 @@
       this._super( key, value );
     },
 
-    refresh: function() {
-      var $columns = $( '.pivot-row th, .content td' );
-      //$columns.css( 'width', this.options.colWidth );
-      $columns.css( 'min-width', this.options.colWidth );
+    horizontalMerge: function( tds ) {
+      var i = 1,
+          len;
 
+      if ( !$.isArray( tds ) ) {
+        tds = arguments;
+      }
+
+      len = tds.length;
+
+      for ( ; i < len; i++ ) {
+        $( tds[ i ], this.el.$this ).remove();
+      }
+
+      $( tds[ 0 ], this.el.$this ).attr( 'colspan', len );
+    },
+
+    refresh: function() {
+      var $columns = $( '.content td' );
+      var $headers = $( '.pivot-row th' );
+      
+      if ( !this.options.ignoreColWidth ) {
+	      $columns.css( 'width', this.options.colWidth );
+	      $columns.css( 'min-width', this.options.colWidth );
+      }
+      
+      $headers.css( 'width', this.options.colWidth );
+      $headers.css( 'min-width', this.options.colWidth );
+      
       $( '.pivot-column-scroller, .fixed-corner-left' ).css( 'width', this.options.pivotColWidth );
       $( '.pivot-row-scroller, .content-scroller' ).css( 'left', this.options.pivotColWidth );
     }
